52be576dff777dc9b3b99c8b969dc3065737f431
Dennis Nemirovski <dennisnemirovski@gmail.com>
2021-01-14 16:45:51 +0200

    logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,14 @@ import rootReducer from "./root-reducer";
 import { persistStore } from 'redux-persist';
 
 const middlewares = [logger];
+if (process.env.NODE_ENV==='development'){
+    middlewares.push(logger)
+}
+
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+
+
 const persistor = persistStore(store)
+
 export  { store, persistor };
